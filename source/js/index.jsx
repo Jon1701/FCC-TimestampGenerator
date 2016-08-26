6d321c932c513781c45ce45ed542e35edb5b82ee
Jon Balon <development@jonbalon.com>
2016-08-26 17:29:45 -0400

    Added Tab functionality

--- source/js/index.jsx
+++ source/js/index.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import TabContainer from './TabContainer.jsx';
+
 class UserInterface extends React.Component {
+
+  handleActiveTabChange(tabId) {
+    this.setState({
+      activeTab: tabId
+    });
+  }
 
   // Component constructor.
   constructor() {
     super();
+
+    // Default state.
+    this.state = {
+      activeTab: 0  // Currently active tab.
+    }
   }
 
   // Component render.
@@ -13,7 +26,7 @@ class UserInterface extends React.Component {
     return (
       <div>
 
-        Hello World
+        <TabContainer activeTab={this.state.activeTab} handleTabClick={this.handleActiveTabChange.bind(this)}/>
 
       </div>
     )
